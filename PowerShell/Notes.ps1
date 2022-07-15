9890755f2ecd112812610c40c0421aa9f40ca83b
Von Gong <vgong@batteriesplus.com>
2022-07-15 16:05:11 -0500

    upd ps1

--- PowerShell/Notes.ps1
+++ PowerShell/Notes.ps1
@@ -1,6 +1,9 @@
 
+ Get-Alias
+ 
 #Select-Object
 Get-Process | Select-Object -Property ProcessName, Id, WS
 Get-Process Explorer | Select-Object -Property ProcessName -ExpandProperty Modules | Format-List
 Get-Process | Sort-Object -Property WS | Select-Object -Last 5
 "a","b","c","a","a","a" | Select-Object -Unique
+
