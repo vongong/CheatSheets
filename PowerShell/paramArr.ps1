469be9e4f37576211eb0d262776a96d0713a9ba1
Von Gong <vgong@batteriesplus.com>
2022-08-01 14:49:08 -0500

    ps upd

--- PowerShell/paramArr.ps1
+++ PowerShell/paramArr.ps1
@@ -11,15 +11,20 @@ param (
 
 # Write-Host $Environment
 # Write-Host $Region
+$ValidRg = "north", "south"
 
 foreach ($Env in $Environment) {
     if ([string]::IsNullOrWhiteSpace($Env)) {
-        Write-Host "Cannot have empty Environment"
+        Write-Host "Cannot have empty Environment" -ForegroundColor "Red"
         continue
     }
     foreach ($Rg in $Region) {
         if ([string]::IsNullOrWhiteSpace($Rg)) {
-            Write-Host "Cannot have empty Region"
+            Write-Host "Cannot have empty Region" -ForegroundColor "Red"
+            continue
+        }
+        if ($Rg.ToLower() -notin $ValidRg) {
+            Write-Host "$Rg not valid Region" -ForegroundColor "Red"
             continue
         }
         Write-Host "$Env-$Rg-app"
