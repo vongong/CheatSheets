5c22ad298b60376beec70841d40c728e38a61ec7
Von Gong <vgong@batteriesplus.com>
2022-08-02 10:03:18 -0500

    add color

---
+++ PowerShell/color.ps1
@@ -0,0 +1,6 @@
+Write-Host "Whole text is in green" -ForegroundColor Green
+Write-Host "Whole text is in red" -ForegroundColor Red
+Write-Host "Whole text is in white" -ForegroundColor White
+Write-Host "Green " -ForegroundColor Green -NoNewline; 
+Write-Host "Red " -ForegroundColor Red -NoNewline; 
+Write-Host "Yellow " -ForegroundColor Yellow;
