60e905b851ce91c4a1b999ae504b7b94151a253c
Von Gong <vgong@batteriesplus.com>
2022-08-18 09:34:46 -0500

    using for remote command

--- PowerShell/foreach.ps1
+++ PowerShell/foreach.ps1
@@ -6,3 +6,9 @@ $urls = "dev.site.com",
 $urls | Foreach-Object -ThrottleLimit 5 -Parallel {
     Write-Host "process url: $_"
 }
+
+$s = '111','222','aaa','bbb','ccc'
+# error
+# 0..4 | ForEach-Object -Parallel { $s[$_] }
+
+0..4 | ForEach-Object -Parallel {$($using:s)[$_] }
