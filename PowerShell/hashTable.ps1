119f34fd7e8dfb533f41ad37ff892301553b9963
Von Gong <vgong@batteriesplus.com>
2022-11-18 13:27:39 -0600

    add menu ps1

---
+++ PowerShell/hashTable.ps1
@@ -0,0 +1,17 @@
+$hadrMap = @{}
+
+$AppObj = New-Object -TypeName PSObject
+$AppObj | Add-Member -Name 'North' -MemberType NoteProperty -Value 'NValue1'
+$AppObj | Add-Member -Name 'South' -MemberType NoteProperty -Value 'SValue1'
+$hadrMap['d07'] = $AppObj
+
+$AppObj = New-Object -TypeName PSObject
+$AppObj | Add-Member -Name 'North' -MemberType NoteProperty -Value 'NValue2'
+$AppObj | Add-Member -Name 'South' -MemberType NoteProperty -Value 'SValue2'
+$hadrMap['t07'] = $AppObj
+
+Write-Host 'hadrMap[d07].North='$hadrMap['d07'].North
+Write-Host 'hadrMap[d07].South='$hadrMap['d07'].South
+
+Write-Host 'hadrMap[t07].North='$hadrMap['t07'].North
+Write-Host 'hadrMap[t07].South='$hadrMap['t07'].South
