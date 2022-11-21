db6760c1fd2e8384109f0a263d8271a00a98ace3
Von Gong <vgong@batteriesplus.com>
2022-11-21 09:56:32 -0600

    upd csv

--- PowerShell/csv/csv.ps1
+++ PowerShell/csv/csv.ps1
@@ -1,5 +1,17 @@
+function test-arr{
+    param(
+        [object[]] $testArr
+    )
+
+    Foreach ($app in $testArr) {
+        Write-Host "from func App=$($app.App) RG=$($app.RG)"
+    }
+}
+
 $AppList = Import-CSV -path "applist.csv"
 
 Foreach ($app in $AppList) {
-    Write-Host "App=$($app.App) RG=$($app.RG)"
+    Write-Host "inline App=$($app.App) RG=$($app.RG)"
 }
+
+test-arr($AppList)
