5fe75cca400bf5c44582329a6d08eb9176819540
Von Gong <vgong@batteriesplus.com>
2022-11-28 09:13:07 -0600

    add write-test.ps1

---
+++ PowerShell/write-test.ps1
@@ -0,0 +1,31 @@
+Write-Host 'Write Test'
+<#
+.\write-test.ps1
+.\write-test.ps1 -verbose
+.\write-test.ps1 -debug
+.\write-test.ps1 -verbose -debug
+
+#>
+param(
+    [switch]$Verbose,
+    [switch]$Debug
+)
+
+
+# save the current preferences to restore later
+$CurrentVerbosePreference = $VerbosePreference
+$CurrentDebugPreference = $DebugPreference
+if ($Verbose){
+    $VerbosePreference = 'Continue'
+}
+if ($Debug){
+    $DebugPreference = 'Continue'
+}
+
+Write-Verbose 'This is verbose'
+Write-Debug 'This is debug'
+Write-Warning 'This is a warning'
+Write-Error 'This is an error'
+
+$VerbosePreference = $CurrentVerbosePreference
+$DebugPreference = $CurrentDebugPreference
